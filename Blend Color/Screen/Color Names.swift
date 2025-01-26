88e34a6aaf50ee1187b5137eca8c46403cdbcad0
Nadezhda <el.shtolts@yandex.ru>
2025-01-26 23:45:35 +0300

    added backgroundColor + update design

--- Blend Color/Screen/Color Names.swift
+++ Blend Color/Screen/Color Names.swift
@@ -36,5 +36,25 @@ let colorNames: [String: UIColor] = [
     "Olive": UIColor(red: 128/255, green: 128/255, blue: 0/255, alpha: 1.0), // Оливковый
     "Maroon": UIColor(red: 128/255, green: 0/255, blue: 0/255, alpha: 1.0), // Бордовый
     "SlateGray": UIColor(red: 112/255, green: 128/255, blue: 144/255, alpha: 1.0), // Сланцевый серый
-    "Turquoise": UIColor(red: 64/255, green: 224/255, blue: 208/255, alpha: 1.0) // Бирюзовый
+    "Turquoise": UIColor(red: 64/255, green: 224/255, blue: 208/255, alpha: 1.0), // Бирюзовый
+    "Periwinkle": UIColor(red: 204/255, green: 204/255, blue: 255/255, alpha: 1.0), // Барвинок
+    "Sienna": UIColor(red: 160/255, green: 82/255, blue: 45/255, alpha: 1.0), // Сиена
+    "Crimson": UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1.0), // Малиновый
+    "Amber": UIColor(red: 255/255, green: 191/255, blue: 0/255, alpha: 1.0), // Янтарный
+    "Beige": UIColor(red: 245/255, green: 245/255, blue: 220/255, alpha: 1.0), // Бежевый
+    "Chartreuse": UIColor(red: 127/255, green: 255/255, blue: 0/255, alpha: 1.0), // Шартрез
+    "Aqua": UIColor(red: 0/255, green: 255/255, blue: 255/255, alpha: 1.0), // Аквамарин
+    "Fuchsia": UIColor(red: 255/255, green: 0/255, blue: 255/255, alpha: 1.0), // Фуксия
+    "Khaki": UIColor(red: 240/255, green: 230/255, blue: 140/255, alpha: 1.0), // Хаки
+    "Lime": UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1.0), // Лайм
+    "Plum": UIColor(red: 221/255, green: 160/255, blue: 221/255, alpha: 1.0), // Сливовый
+    "Orchid": UIColor(red: 218/255, green: 112/255, blue: 214/255, alpha: 1.0), // Орхидея
+    "Tomato": UIColor(red: 255/255, green: 99/255, blue: 71/255, alpha: 1.0), // Томато
+    "Azure": UIColor(red: 0/255, green: 127/255, blue: 255/255, alpha: 1.0), // Лазурный
+    "Mauve": UIColor(red: 224/255, green: 176/255, blue: 255/255, alpha: 1.0), // Розовато-лиловый
+    "Peru": UIColor(red: 205/255, green: 133/255, blue: 63/255, alpha: 1.0), // Перуанский
+    "Seashell": UIColor(red: 255/255, green: 245/255, blue: 238/255, alpha: 1.0), // Ракушечный
+    "Thistle": UIColor(red: 216/255, green: 191/255, blue: 216/255, alpha: 1.0), // Чертополох
+    "Wheat": UIColor(red: 245/255, green: 222/255, blue: 179/255, alpha: 1.0) // Пшеничный
 ]
+
--- Blend Color/Screen/ColorMixingViewController.swift
+++ Blend Color/Screen/ColorMixingViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ColorMixingViewController: UIViewController, UIColorPickerViewControllerDelegate {
 
+    
+    // MARK: - UI elements
+    
+    private lazy var backgroundView: UIImageView = {
+        let element = UIImageView(image: UIImage(named: "background"))
+        element.contentMode = .scaleAspectFill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.text = "Mix Colors 🎨"
+        element.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        element.textColor = UIColor.white
+        element.textAlignment = .center
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+
     private lazy var firsColorButton: UIButton = {
         let element = UIButton(type: .system)
         element.backgroundColor = .red
-        element.layer.cornerRadius = 75
+        element.layer.cornerRadius = 84
         element.setTitle("Red", for: .normal)
         element.setTitleColor(.white, for: .normal)
         element.tag = 0 // Set tag for first color button
@@ -25,7 +46,7 @@ class ColorMixingViewController: UIViewController, UIColorPickerViewControllerDe
         element.backgroundColor = .blue
         element.setTitle("Blue", for: .normal)
         element.setTitleColor(.white, for: .normal)
-        element.layer.cornerRadius = 75
+        element.layer.cornerRadius = 84
         element.tag = 1 // Set tag for second color button
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
@@ -36,7 +57,7 @@ class ColorMixingViewController: UIViewController, UIColorPickerViewControllerDe
         element.backgroundColor = .purple
         element.setTitle("Purple", for: .normal)
         element.setTitleColor(.black, for: .normal)
-        element.layer.cornerRadius = 75
+        element.layer.cornerRadius = 84
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
@@ -44,13 +65,16 @@ class ColorMixingViewController: UIViewController, UIColorPickerViewControllerDe
     private var selectedButton: UIButton?
     var color1: UIColor? = .red
     var color2: UIColor? = .blue
-
+    
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.addSubview(backgroundView)
         view.addSubview(firsColorButton)
         view.addSubview(secondColorButton)
         view.addSubview(resultsButton)
+        view.addSubview(titleLabel)
+       
         setupConstraints()
         updateMixedColor()
     }
@@ -177,21 +201,29 @@ extension ColorMixingViewController {
     
     func setupConstraints() {
         NSLayoutConstraint.activate(
-            [firsColorButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            [ backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+              backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+              backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+              backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                
+             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+             
+             firsColorButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
              firsColorButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-             firsColorButton.heightAnchor.constraint(equalToConstant: 150),
-             firsColorButton.widthAnchor.constraint(equalToConstant: 150),
+             firsColorButton.heightAnchor.constraint(equalToConstant: 168),
+             firsColorButton.widthAnchor.constraint(equalToConstant: 168),
              
              secondColorButton.topAnchor.constraint(equalTo: firsColorButton.topAnchor),
-             secondColorButton.leadingAnchor.constraint(equalTo: firsColorButton.trailingAnchor, constant: -15),
+             secondColorButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             
-             secondColorButton.heightAnchor.constraint(equalToConstant: 150),
-             secondColorButton.widthAnchor.constraint(equalToConstant: 150),
+             secondColorButton.heightAnchor.constraint(equalToConstant: 168),
+             secondColorButton.widthAnchor.constraint(equalToConstant: 168),
 
              resultsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
              resultsButton.topAnchor.constraint(equalTo: firsColorButton.bottomAnchor, constant: -50),
-             resultsButton.heightAnchor.constraint(equalToConstant: 150),
-             resultsButton.widthAnchor.constraint(equalToConstant: 150),
+             resultsButton.heightAnchor.constraint(equalToConstant: 168),
+             resultsButton.widthAnchor.constraint(equalToConstant: 168),
             ]
 )
 
